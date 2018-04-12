386257aaada4eb1872a40e8f1e6506d516837be3
lanigraham <35030402+lanigraham@users.noreply.github.com>
2018-04-12 23:59:14 +0100

    NEW COMPONENT (#1)
    * added a new component
    
    * added new component

---
+++ src/Components/Jimmy.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Jimmy extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <p>This is coming from the Jimmy Component.</p>
+      </div>
+    );
+  }
+}
+
+export default Jimmy;
